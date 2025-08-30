5673afacbe7c944507dd3997c02372e94a82143b
rmotta.net:~# <114815819+rmottanet@users.noreply.github.com>
2025-08-30 13:37:45 -0300

    Enhance (#1)
    * Update repo-action.sh
    
    * Update action.yml
    
    * Update README.md

--- repo-action.sh
+++ repo-action.sh
@@ -6,23 +6,49 @@ OWNER="$2"
 REPO_NAME="$3"
 DESCRIPTION="$4"
 VISIBILITY="$5"
+TEAM="$6"
+ADD_README="$7"
+TEMPLATE="$8"
 
+# Checking mandatory parameters
 if [[ -z "$ACTION" || -z "$OWNER" || -z "$REPO_NAME" ]]; then
-  echo "Usage: $0 <action> <owner> <repo_name> [<description>] [<visibility>]"
+  echo "Uso: $0 <action> <owner> <repo_name> [<description>] [<visibility>] [<team>] [<add_readme>] [<template>]"
+  echo "Exemplos:"
+  echo "  ./repo-manager.sh create my-org my-new-repo \"An incredible project!\" public my-team true"
+  echo "  ./repo-manager.sh create my-org template-project \"A new description\" -- -- \"\" my-org/my-template-repo"
   exit 1
 fi
 
 REPO="${OWNER}/${REPO_NAME}"
 
-# Inicializa o comando base e os argumentos
+# Initializes the base command and arguments
 COMMAND_ARGS=( "$ACTION" "$REPO" )
 
 case "$ACTION" in
   "create")
+
     if [[ -n "$DESCRIPTION" ]]; then
       COMMAND_ARGS+=( "--description" "$DESCRIPTION" )
     fi
-    # Adiciona a flag de visibilidade apenas se necessário
+    
+    # Checking restrictions on the --template parameter
+    if [[ -n "$TEMPLATE" ]]; then
+      if [[ "$ADD_README" == "true" || -n "$TEAM" ]]; then
+        echo "Erro: O parâmetro --template não pode ser usado com --add-readme ou --team."
+        exit 1
+      fi
+      COMMAND_ARGS+=( "--template" "$TEMPLATE" )
+    else
+      # Add the optional parameters only if you are not using --template
+      if [[ "$ADD_README" == "true" ]]; then
+        COMMAND_ARGS+=( "--add-readme" )
+      fi
+
+      if [[ -n "$TEAM" ]]; then
+        COMMAND_ARGS+=( "--team" "$TEAM" )
+      fi
+    fi
+
     if [[ "$VISIBILITY" == "private" ]]; then
       COMMAND_ARGS+=( "--private" )
     elif [[ "$VISIBILITY" == "internal" ]]; then
@@ -33,13 +59,13 @@ case "$ACTION" in
     COMMAND_ARGS+=( "--yes" )
     ;;
   *)
-    echo "Error: Unknown action '$ACTION'."
+    echo "Erro: Ação desconhecida '$ACTION'."
     exit 1
     ;;
 esac
 
-# Executa o comando gh com os argumentos do array
-echo "Running: gh repo ${COMMAND_ARGS[@]}"
+# Runs the gh command with the array arguments
+echo "Executando: gh repo ${COMMAND_ARGS[@]}"
 OUTPUT=$(gh repo "${COMMAND_ARGS[@]}" 2>&1)
 STATUS=$?
 
